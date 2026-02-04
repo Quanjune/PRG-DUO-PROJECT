dac749e95d920e9d23c4f20e017dd78036174d1b
kishore2007 <rajakumarkishore28@gmail.com>
2026-02-04 12:46:50 +0800

    food item class

--- S10268576_PRG2Assignment/S10268576_PRG2Assignment/FoodItem.cs
+++ S10268576_PRG2Assignment/S10268576_PRG2Assignment/FoodItem.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace S10268576_PRG2Assignment
+﻿public class FoodItem
 {
-    internal class FoodItem
+    public string ItemName { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+
+    public FoodItem(string itemName, string description, double price)
     {
+        ItemName = itemName;
+        Description = description;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        return $"{ItemName}: {Description} - ${Price:F2}";
     }
 }
