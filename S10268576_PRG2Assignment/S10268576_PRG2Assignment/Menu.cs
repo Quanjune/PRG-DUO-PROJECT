f25f92ca8b0bc29080771e81810de02eddaa5b89
kishore2007 <rajakumarkishore28@gmail.com>
2026-02-04 12:47:31 +0800

    menu class

--- S10268576_PRG2Assignment/S10268576_PRG2Assignment/Menu.cs
+++ S10268576_PRG2Assignment/S10268576_PRG2Assignment/Menu.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace S10268576_PRG2Assignment
+﻿public class Menu
 {
-    internal class Menu
+    public string MenuId { get; set; }
+    public string MenuName { get; set; }
+    public List<FoodItem> FoodItems { get; set; }
+
+    public Menu(string menuId, string menuName)
     {
+        MenuId = menuId;
+        MenuName = menuName;
+        FoodItems = new List<FoodItem>();
+    }
+
+    public void AddFoodItem(FoodItem item)
+    {
+        FoodItems.Add(item);
     }
 }
