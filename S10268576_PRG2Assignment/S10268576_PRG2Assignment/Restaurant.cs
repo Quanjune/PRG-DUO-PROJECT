111bbbd36b231db064e4c84fd73742e6e0bb30fa
kishore2007 <rajakumarkishore28@gmail.com>
2026-02-04 12:48:36 +0800

    restaruent class

--- S10268576_PRG2Assignment/S10268576_PRG2Assignment/Restaurant.cs
+++ S10268576_PRG2Assignment/S10268576_PRG2Assignment/Restaurant.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace S10268576_PRG2Assignment
+﻿public class Restaurant
 {
-    internal class Restaurant
+    public string RestaurantId { get; set; }
+    public string RestaurantName { get; set; }
+    public string RestaurantEmail { get; set; }
+
+    public Menu Menu { get; set; }
+    public List<SpecialOffer> SpecialOffers { get; set; }
+
+    public Restaurant(string id, string name, string email)
     {
+        RestaurantId = id;
+        RestaurantName = name;
+        RestaurantEmail = email;
+        Menu = new Menu(id, $"{name} Menu");
+        SpecialOffers = new List<SpecialOffer>();
+    }
+
+    public override string ToString()
+    {
+        return $"Restaurant: {RestaurantName} ({RestaurantId})";
     }
 }
