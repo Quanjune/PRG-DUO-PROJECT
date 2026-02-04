d241c14ed89bcde4a063b2bd6a6542ef4c00f5a3
kishore2007 <rajakumarkishore28@gmail.com>
2026-02-04 12:48:10 +0800

    special offer class

--- S10268576_PRG2Assignment/S10268576_PRG2Assignment/SpecialOffer.cs
+++ S10268576_PRG2Assignment/S10268576_PRG2Assignment/SpecialOffer.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace S10268576_PRG2Assignment
+﻿public class SpecialOffer
 {
-    internal class SpecialOffer
+    public string OfferCode { get; set; }
+    public string Description { get; set; }
+    public double Discount { get; set; }
+
+    public SpecialOffer(string offerCode, string description, double discount)
     {
+        OfferCode = offerCode;
+        Description = description;
+        Discount = discount;
+    }
+
+    public override string ToString()
+    {
+        return $"{OfferCode} - {Description} ({Discount}%)";
     }
 }
